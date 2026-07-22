4b596068ad0e8ab4d80183ce83a4da6cf901b542
Ketan Dholakia <ketanmci@gmail.com>
2026-07-22 11:54:40 +0530

    fix: constrain fallback theme icon size

--- resources/views/admin/themes/index.blade.php
+++ resources/views/admin/themes/index.blade.php
@@ -15,8 +15,8 @@
                 @if(file_exists($theme['path'] . '/screenshot.png'))
                     <img src="{{ asset('themes/' . $theme['id'] . '/screenshot.png') }}" class="w-full h-full object-cover">
                 @else
-                    <div class="absolute inset-0 flex items-center justify-center text-gray-400">
-                        <svg class="w-12 h-12 opacity-50" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
+                    <div class="absolute inset-0 flex items-center justify-center text-gray-400 overflow-hidden">
+                        <svg width="48" height="48" class="opacity-50" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                     </div>
                 @endif
                 
